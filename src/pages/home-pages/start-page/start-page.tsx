3f1d5baef2069530e6200f8d9057fc9eda568436
XAITYH <petrovskijpavel1@gmail.com>
2024-10-09 22:12:30 +0300

    start-page

--- src/pages/home-pages/start-page/start-page.tsx
+++ src/pages/home-pages/start-page/start-page.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import styles from "./start-page.module.scss";
-import Link from "next/link";
+import React from 'react';
+import styles from './start-page.module.scss';
+import Image from 'next/image';
+import programming from '../../../shared/assets/icons/programming.svg';
+import arrowUp from '../../../shared/assets/icons/arrow-up.svg';
+import Link from 'next/link';
 
 export const StartPage = () => {
-  return (
-    <div className={styles.startPage}>
-        <Link href={"#"} className={styles.testButton}>Пройти тестирование</Link>
-    </div>
-  );
-};
+	return (
+		<div className={styles.startPage}>
+			<Image
+				src={programming}
+				className={styles.programming}
+				alt='programming'
+			/>
+			<div className={styles.texts}>
+				<h1 className={styles.schoolPhraze}>
+					Школа финансовой грамоты FINCOF - для тех, кому важен результат
+				</h1>
+				<p className={styles.testParagraph}>
+					Пройдите тестирование, чтобы получить доступ к вводным урокам
+				</p>
+				<Link href={'/quiz-page'} className={styles.testButton}>
+					Пройти тестирование
+					<Image src={arrowUp} className={styles.arrowUp} alt='arrow-up' />
+				</Link>
+			</div>
+		</div>
+	);
+}
+
