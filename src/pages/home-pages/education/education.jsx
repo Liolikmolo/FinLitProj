09a1089b113f5f25fafb0088825f4b2fd197d89e
XAITYH <petrovskijpavel1@gmail.com>
2024-10-11 16:17:17 +0300

    added 4

---
+++ src/pages/home-pages/education/education.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './landing-education.module.scss';
+import LandingButton from "@/shared/ui/buttons/landing-button/landing-button";
+
+export const LandingEducation = () => {
+	return (
+		<div id='education' className={styles.educationWrapper}>
+			<div className={styles.edObjects}>
+				<h2 className={styles.howWeTeach}>Как у нас происходит обучение?</h2>
+				<p className={styles.ourProgram}>
+					Обучение должно быть комфортным. Поэтому мы разработали собственную
+					платформу для обучения финансовой грамоте. На ней вы можете не только
+					изучать теорию, но и выполнять интерактивные практические задания.
+				</p>
+				<div className={styles.top3}>
+					<div className={styles.top}>
+						<h1 className={styles.num}>01</h1>
+						<h2 className={styles.title}>
+							Весь материал разбит на небольшие уроки
+						</h2>
+						<p className={styles.description1}>
+							Теория и практика подаются маленькими порциями. Так вам будет
+							легче усваивать новые знания.
+						</p>
+					</div>
+					<div className={styles.top}>
+						<h1 className={styles.num}>02</h1>
+						<h2 className={styles.title}>Обучение через практику</h2>
+						<p className={styles.description2}>
+							Всё, что вы узнали, вы тут же начинаете применять на практике. Вы
+							сразу видите результаты своего труда.
+						</p>
+					</div>
+					<div className={styles.top}>
+						<h1 className={styles.num}>03</h1>
+						<h2 className={styles.title}>Нет ограничений по времени</h2>
+						<p className={styles.description3}>
+							Можно совмещать учёбу с другими дeлами. Не нужно выпрашивать
+							отпуск, если пришлось сделать перерыв.
+						</p>
+					</div>
+				</div>
+				<div className={styles.startEducation}>
+					<LandingButton width={249} height={70}>
+						Начать обучение
+					</LandingButton>
+					<p className={styles.testStartText}>
+						Попробуйте сами первые уроки, но нужно пройти тестирование
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/pages/home-pages/footer/footer.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import styles from './landing-footer.module.scss';
+import Image from 'next/image';
+import LandingButton from "@/shared/ui/buttons/landing-button/landing-button";
+import loudspeaker from '@/shared/assets/icons/loudspeaker.svg';
+import logo from '@/shared/assets/icons/logo.svg';
+import instagram from '@/shared/assets/icons/instagram.svg';
+import vk from '@/shared/assets/icons/vk.svg';
+import facebook from '@/shared/assets/icons/facebook.svg';
+import youtube from '@/shared/assets/icons/youtube.svg';
+import Link from 'next/link';
+
+export const LandingFooter = () => {
+	return (
+		<footer id='footer' className={styles.footer}>
+			<div className={styles.footerObjects}>
+				<div className={styles.warningContainer}>
+					<Image src={loudspeaker} alt='loudspeaker-image' />
+					<p className={styles.warning}>
+						Все блоки проходятся строго по порядку. Пропустить какой-то блок или
+						начать обучение с середины нельзя, даже если вы считаете, что уже
+						знаете какую-то часть материала. Только так мы можем гарантировать,
+						что вы получите все знания, предусмотренные учебной программой.
+					</p>
+					<LandingButton width={311} height={70} fontSize={20}>
+						Начать обучение
+					</LandingButton>
+				</div>
+				<div className={styles.information}>
+					<div className={styles.links_school}>
+						<div className={styles.school_container}>
+							<Image className={styles.logo} src={logo} alt='logotype-image' />
+							<p className={styles.school}>Школа финансовой грамоты</p>
+						</div>
+						<div className={styles.soical_media_links}>
+							<Image
+								className={styles.social_media}
+								src={instagram}
+								alt='instagram-image'
+							/>
+							<Image className={styles.social_media} src={vk} alt='vk-image' />
+							<Image
+								className={styles.social_media}
+								src={facebook}
+								alt='facebook-image'
+							/>
+							<Image
+								className={styles.social_media}
+								src={youtube}
+								alt='youtube-image'
+							/>
+						</div>
+					</div>
+					<ul className={styles.linksUl}>
+						<li className={styles.linksLi}>
+							<Link className={styles.links} href='#knowledge'>Чему вы научитесь</Link>
+						</li>
+						<li className={styles.linksLi}>
+							<Link className={styles.links} href='#education'>Процесс обучения</Link>
+						</li>
+						<li className={styles.linksLi}>
+							<Link className={styles.links} href='#footer'>Контакты</Link>
+						</li>
+						<li className={styles.linksLi}>
+							<Link className={styles.links} href='#header'>Регистрация</Link>
+						</li>
+					</ul>
+					<div className={styles.contact_info}>
+						<Link href='/' className={styles.e_mail}>
+							info@fincof.by
+						</Link>
+						<p className={styles.tel_number}>+375 99 999 99 99</p>
+						<p className={styles.organization}>Независимая организация</p>
+					</div>
+				</div>
+				<div className={styles.last_info}>
+					<p className={styles.copyright}>© 2024 FINCOF — Все права защищены</p>
+					<a className={styles.user_agreement}>Пользовательское соглашение</a>
+					<a className={styles.privacy_policy}>Политика конфиденциальности</a>
+				</div>
+			</div>
+		</footer>
+	);
+}
