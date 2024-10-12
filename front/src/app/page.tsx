4e3fb1847273bd0b18a9aa8b0ad5c43fd6aca1a9
Liolik <aleksey.taratuta@gmail.com>
2024-10-12 19:29:20 +0300

    added prisma and DB

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,11 @@
-export default function Home() {
-  return (
-    <h1>Hello</h1>
-  );
+import prisma from '@/lib/prisma';
+
+export default async function Home() {
+	const data = await prisma.question.findMany();
+	const content = data.map((item) => {
+		const text = item.content;
+		return text;
+	});
+
+	return <h1>{content}</h1>;
 }
