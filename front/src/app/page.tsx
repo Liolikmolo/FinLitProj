66941b6e964392d2ae203fec36c73f752d774a1f
Liolik <aleksey.taratuta@gmail.com>
2024-10-20 19:11:26 +0300

    bug fix

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,7 @@
-import { SessionProvider } from 'next-auth/react';
-import { AppProps } from 'next/app';
 import { Landing } from '@/pages/landing-page';
 
-const Home = ({ pageProps }: AppProps) => {
-	return (
-		<SessionProvider>
-			<Landing {...pageProps} />
-		</SessionProvider>
-	);
+const Home = () => {
+	return <Landing />;
 };
 
 export default Home;
