e746784a1303a544256d6d5c6a1a079d10344812
XAITYH <petrovskijpavel1@gmail.com>
2024-10-21 19:04:20 +0300

    changed: question-tab styles (in main)

--- front/src/widgets/header/header.tsx
+++ front/src/widgets/header/header.tsx
@@ -5,9 +5,17 @@ import logo from '@/shared/assets/icons/logo.svg';
 import arrow from '@/shared/assets/icons/arrow.svg';
 import Link from 'next/link';
 
-export const Header = () => {
+type HeaderProps = {
+	backgroundColor?: string,
+};
+
+export const Header = ({backgroundColor='#eaf2f5'} : HeaderProps) => {
+	const headerStyle = {
+		backgroundColor: backgroundColor,
+	};
+
 	return (
-		<header id="header" className={styles.header}>
+		<header id="header" className={styles.header} style={headerStyle}>
 			<Image src={logo} className={styles.logo} alt="logotype" />
 			<ul className={styles.links}>
 				<li className={styles.links_item}>
@@ -24,10 +32,10 @@ export const Header = () => {
 				</li>
 			</ul>
 			<div className={styles.buttons}>
-				<Link href="/registration">
+				<Link href="registration">
 					<button className={styles.registrationButton}>Регистрация</button>
 				</Link>
-				<Link href="/login">
+				<Link href="login">
 					<button className={styles.loginButton}>
 						<Image src={arrow} className={styles.arrow} alt="arrow" /> Войти
 					</button>
